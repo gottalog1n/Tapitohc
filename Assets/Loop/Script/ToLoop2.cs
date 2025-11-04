bd13ab8ed91b12ff18f2d4c98a8301f68668302c
natchanon12 <68070249@kmitl.ac.th>
2025-11-04 21:45:45 +0700

    Script for Loop room

---
+++ Assets/Loop/Script/ToLoop2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLoop2 : MonoBehaviour
+{
+    public Animator fade;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered trigger!");
+            StartCoroutine(Fading());
+        }
+    }
+
+    IEnumerator Fading()
+    {
+        Debug.Log("Start Fading coroutine");
+        if (fade != null)
+        {
+            fade.SetBool("Fade", true);
+        }
+        else
+        {
+            Debug.LogWarning("No Animator assigned to 'fade'!");
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log("Loading scene 'Loop 2'...");
+        SceneManager.LoadScene("Loop 2");
+    }
+}
